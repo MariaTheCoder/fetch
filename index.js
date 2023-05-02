36c1363a451128d24a14765cb13a7520d2f175cd
Maria T. Andersen <mteilmann93@gmail.com>
2023-05-02 14:17:23 +0200

    create a new user

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ getUsers("https://jsonplaceholder.typicode.com/users/");
  * Get a single user
  */
 getUsers("https://jsonplaceholder.typicode.com/users/2");
+
+/**
+ * Create a new user
+ */
+function createUser(url) {
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Maria Teilmann Andersen",
+      phone: "045-22743457",
+      username: "DanishViking",
+      website: "http://www.somrevisor.dk",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+createUser("https://jsonplaceholder.typicode.com/users");

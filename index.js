7918af1461f58a5ad88b9a71999dee65c4b4fa4d
Maria T. Andersen <mteilmann93@gmail.com>
2023-05-02 14:17:05 +0200

    rename function

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * Get all users
  */
-function users(url) {
+function getUsers(url) {
   fetch(url)
     .then((response) => response.json())
     .then((json) => console.log(json));
 }
-users("https://jsonplaceholder.typicode.com/users/");
+getUsers("https://jsonplaceholder.typicode.com/users/");
 
 /**
  * Get a single user
  */
-users("https://jsonplaceholder.typicode.com/users/2");
+getUsers("https://jsonplaceholder.typicode.com/users/2");

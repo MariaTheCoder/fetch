403b4b74a92aab9a099cd051a6852c1accbe6313
Maria T. Andersen <mteilmann93@gmail.com>
2023-05-02 13:46:06 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * Get all users
  */
-function posts(url) {
+function users(url) {
   fetch(url)
     .then((response) => response.json())
     .then((json) => console.log(json));
 }
-posts("https://jsonplaceholder.typicode.com/users/");
+users("https://jsonplaceholder.typicode.com/users/");
 
 /**
  * Get a single user
  */
-posts("https://jsonplaceholder.typicode.com/users/2");
+users("https://jsonplaceholder.typicode.com/users/2");
